6497790911aaf7d6e1db80fe66125067923f8dbb
Vladimir Rusanov <Rusanov.Vladimir@gmail.com>
2016-12-03 17:30:32 +0200

    Initial commit

---
+++ AsyncCommand/CancelCommand.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Nito.Disposables;
+
+namespace Nito.Mvvm
+{
+    /// <summary>
+    /// A command that cancels a <see cref="CancellationToken"/> when it is executed. "Operations" may be started for this command. This command is canceled whenever there are no operations.
+    /// </summary>
+    public sealed class CancelCommand : ICommand
+    {
+        /// <summary>
+        /// The implementation of <see cref="ICommand.CanExecuteChanged"/>.
+        /// </summary>
+        private readonly ICanExecuteChanged _canExecuteChanged;
+
+        /// <summary>
+        /// The cancellation token source currently controlled by this command.
+        /// </summary>
+        private CancellationTokenSource _cts;
+
+        /// <summary>
+        /// The number of current operations. If this is <c>0</c>, then the command is canceled.
+        /// </summary>
+        private int _count;
+
+        /// <summary>
+        /// Creates a new cancel command.
+        /// </summary>
+        /// <param name="canExecuteChangedFactory">The factory for the implementation of <see cref="ICommand.CanExecuteChanged"/>.</param>
+        public CancelCommand(Func<object, ICanExecuteChanged> canExecuteChangedFactory)
+        {
+            _cts = new CancellationTokenSource();
+            _cts.Cancel();
+            _canExecuteChanged = canExecuteChangedFactory(this);
+        }
+
+        /// <summary>
+        /// Creates a new cancel command.
+        /// </summary>
+        public CancelCommand()
+            : this(CanExecuteChangedFactories.DefaultCanExecuteChangedFactory)
+        {
+        }
+
+        /// <summary>
+        /// Gets a cancellation token that will be canceled when this command is executed.
+        /// </summary>
+        public CancellationToken CancellationToken => _cts.Token;
+
+        /// <summary>
+        /// Gets a value indicating whether the cancel command has been executed.
+        /// </summary>
+        public bool IsCancellationRequested => _cts.IsCancellationRequested;
+
+        /// <summary>
+        /// Executes the cancel command.
+        /// </summary>
+        private void Cancel()
+        {
+            _cts.Cancel();
+            _canExecuteChanged.OnCanExecuteChanged();
+        }
+
+        /// <summary>
+        /// Resets this cancel command to an uncanceled state.
+        /// </summary>
+        private void Reset()
+        {
+            _cts = new CancellationTokenSource();
+            _canExecuteChanged.OnCanExecuteChanged();
+        }
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add { _canExecuteChanged.CanExecuteChanged += value; }
+            remove { _canExecuteChanged.CanExecuteChanged -= value; }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return !IsCancellationRequested;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Cancel();
+        }
+
+        /// <summary>
+        /// Decrements the count, and cancels the command if the new count is <c>0</c>.
+        /// </summary>
+        private void Signal()
+        {
+            if (--_count == 0)
+                Cancel();
+        }
+
+        /// <summary>
+        /// Increments the count, and resets the command if the old count was <c>0</c>.
+        /// </summary>
+        private void AddRef()
+        {
+            if (_count++ == 0)
+                Reset();
+        }
+
+        /// <summary>
+        /// Associates an operation with the cancelable command. The operation is disassociated when disposed.
+        /// </summary>
+        public IDisposable StartOperation()
+        {
+            return new SignalOnDispose(this);
+        }
+
+        /// <summary>
+        /// Wraps a delegate so that it registers with this cancel command. The delegate is passed the <see cref="CancellationToken"/> of this cancel command. Any <see cref="OperationCanceledException"/> exceptions raised by the delegate are silently ignored.
+        /// </summary>
+        /// <param name="executeAsync">The cancelable delegate.</param>
+        public Func<object, Task> WrapDelegate(Func<object, CancellationToken, Task> executeAsync)
+        {
+            return async parameter =>
+            {
+                using (StartOperation())
+                {
+                    try
+                    {
+                        await executeAsync(parameter, CancellationToken);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // We cannot use `when (ex.CancellationToken == cancellationToken)` on the catch block because the user delegate may be cancelled by a linked CancellationToken.
+                    }
+                }
+            };
+        }
+
+        /// <summary>
+        /// Wraps a delegate so that it registers with this cancel command. The delegate is passed the <see cref="CancellationToken"/> of this cancel command. Any <see cref="OperationCanceledException"/> exceptions raised by the delegate are silently ignored.
+        /// </summary>
+        /// <param name="executeAsync">The cancelable delegate.</param>
+        public Func<Task> WrapDelegate(Func<CancellationToken, Task> executeAsync)
+        {
+            var wrapped = WrapDelegate((_, token) => executeAsync(token));
+            return () => wrapped(null);
+        }
+
+        private sealed class SignalOnDispose : SingleDisposable<CancelCommand>
+        {
+            public SignalOnDispose(CancelCommand context) : base(context)
+            {
+                context.AddRef();
+            }
+
+            protected override void Dispose(CancelCommand context)
+            {
+                context.Signal();
+            }
+        }
+    }
+}
---
+++ AsyncCommand/CustomAsyncCommand.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Nito.Mvvm
+{
+    /// <summary>
+    /// An asynchronous command where the user determines when it can execute.
+    /// </summary>
+    public sealed class CustomAsyncCommand : AsyncCommandBase, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The implementation of <see cref="IAsyncCommand.ExecuteAsync(object)"/>.
+        /// </summary>
+        private readonly Func<object, Task> _executeAsync;
+
+        /// <summary>
+        /// The implementation of <see cref="ICommand.CanExecute(object)"/>.
+        /// </summary>
+        private readonly Func<object, bool> _canExecute;
+
+        /// <summary>
+        /// Creates a new asynchronous command, with the specified asynchronous delegate as its implementation.
+        /// </summary>
+        /// <param name="executeAsync">The implementation of <see cref="IAsyncCommand.ExecuteAsync(object)"/>.</param>
+        /// <param name="canExecute">The implementation of <see cref="ICommand.CanExecute(object)"/>.</param>
+        /// <param name="canExecuteChangedFactory">The factory for the implementation of <see cref="ICommand.CanExecuteChanged"/>.</param>
+        public CustomAsyncCommand(Func<object, Task> executeAsync, Func<object, bool> canExecute, Func<object, ICanExecuteChanged> canExecuteChangedFactory)
+            : base(canExecuteChangedFactory)
+        {
+            _executeAsync = executeAsync;
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Creates a new asynchronous command, with the specified asynchronous delegate as its implementation.
+        /// </summary>
+        /// <param name="executeAsync">The implementation of <see cref="IAsyncCommand.ExecuteAsync(object)"/>.</param>
+        /// <param name="canExecute">The implementation of <see cref="ICommand.CanExecute(object)"/>.</param>
+        public CustomAsyncCommand(Func<object, Task> executeAsync, Func<object, bool> canExecute)
+            : this(executeAsync, canExecute, CanExecuteChangedFactories.DefaultCanExecuteChangedFactory)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new asynchronous command, with the specified asynchronous delegate as its implementation.
+        /// </summary>
+        /// <param name="executeAsync">The implementation of <see cref="IAsyncCommand.ExecuteAsync(object)"/>.</param>
+        /// <param name="canExecute">The implementation of <see cref="ICommand.CanExecute(object)"/>.</param>
+        /// <param name="canExecuteChangedFactory">The factory for the implementation of <see cref="ICommand.CanExecuteChanged"/>.</param>
+        public CustomAsyncCommand(Func<Task> executeAsync, Func<bool> canExecute, Func<object, ICanExecuteChanged> canExecuteChangedFactory)
+            : this(_ => executeAsync(), _ => canExecute(), canExecuteChangedFactory)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new asynchronous command, with the specified asynchronous delegate as its implementation.
+        /// </summary>
+        /// <param name="executeAsync">The implementation of <see cref="IAsyncCommand.ExecuteAsync(object)"/>.</param>
+        /// <param name="canExecute">The implementation of <see cref="ICommand.CanExecute(object)"/>.</param>
+        public CustomAsyncCommand(Func<Task> executeAsync, Func<bool> canExecute)
+            : this(_ => executeAsync(), _ => canExecute(), CanExecuteChangedFactories.DefaultCanExecuteChangedFactory)
+        {
+        }
+
+        /// <summary>
+        /// Represents the most recent execution of the asynchronous command. Returns <c>null</c> until the first execution of this command.
+        /// </summary>
+        public NotifyTask Execution { get; private set; }
+
+        /// <summary>
+        /// Whether the asynchronous command is currently executing.
+        /// </summary>
+        public bool IsExecuting
+        {
+            get
+            {
+                if (Execution == null)
+                    return false;
+                return Execution.IsNotCompleted;
+            }
+        }
+
+        /// <summary>
+        /// Executes the asynchronous command. Any exceptions from the asynchronous delegate are captured and placed on <see cref="Execution"/>; they are not propagated to the UI loop.
+        /// </summary>
+        /// <param name="parameter">The parameter for the command.</param>
+        public override async Task ExecuteAsync(object parameter)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            Execution = NotifyTask.Create(DoExecuteAsync(tcs.Task, _executeAsync, parameter));
+            var propertyChanged = PropertyChanged;
+            propertyChanged?.Invoke(this, PropertyChangedEventArgsCache.Instance.Get("Execution"));
+            propertyChanged?.Invoke(this, PropertyChangedEventArgsCache.Instance.Get("IsExecuting"));
+            tcs.SetResult(null);
+            await Execution.TaskCompleted;
+            PropertyChanged?.Invoke(this, PropertyChangedEventArgsCache.Instance.Get("IsExecuting"));
+            await Execution.Task;
+        }
+
+        /// <summary>
+        /// Raised when any properties on this instance have changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The implementation of <see cref="ICommand.CanExecute(object)"/>. Invokes the <c>canExecute</c> delegate that was passed to the constructor.
+        /// </summary>
+        /// <param name="parameter">The parameter for the command.</param>
+        protected override bool CanExecute(object parameter) => _canExecute(parameter);
+
+        /// <summary>
+        /// Raises <see cref="ICommand.CanExecuteChanged"/>.
+        /// </summary>
+        public new void OnCanExecuteChanged() => base.OnCanExecuteChanged();
+
+        private static async Task DoExecuteAsync(Task precondition, Func<object, Task> executeAsync, object parameter)
+        {
+            await precondition;
+            await executeAsync(parameter);
+        }
+    }
+}
